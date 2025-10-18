797ce0cb9df64ad6c602b1f6c71e2cdbb570e773
augustgrantham <august.grantham@gmail.com>
2025-10-18 15:33:21 -0500

    add custom resolution and dynamic div resizing

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 //element reference
 const grid = document.querySelector("#container");   
-//generate graph'   
-for(let i = 0; i < 256; i++) {
+const button = document.querySelector("button");
+const root = document.querySelector(":root");
+//button event
+button.addEventListener("click", resize => {
+    grid.innerHTML = "";
+    let newSize = prompt("input desired number of squares per side");
+    if(newSize > 100) {
+        newSize = 100;
+    }
+    generateGraph(newSize);
+    root.style.setProperty("--num-of-squares",newSize);
+});
+//generate graph'  
+function generateGraph(size = 16) {
+for(let i = 0; i < (size * size); i++) {
     const div = document.createElement("div");
     div.addEventListener("mouseenter", draw => {
         div.classList.add("draw");
     });
     grid.appendChild(div);
 }
-
+}
+generateGraph();
