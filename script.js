bb1e94aa763751f3215bf18a327423551b7d89b9
augustgrantham <august.grantham@gmail.com>
2025-10-18 10:24:59 -0500

    add drawing functionality for static sized screen

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const grid = document.querySelector("#container");
-
-//generate graph
-for(let i = 0; i < 65; i++) {
+//element reference
+const grid = document.querySelector("#container");   
+//generate graph'   
+for(let i = 0; i < 256; i++) {
     const div = document.createElement("div");
+    div.addEventListener("mouseenter", draw => {
+        div.classList.add("draw");
+    });
     grid.appendChild(div);
 }
+
