3214a5736312f929ea36d7fce2521f58c01803bc
augustgrantham <august.grantham@gmail.com>
2025-10-18 16:31:20 -0500

    add random color generation and opacity gradient

--- script.js
+++ script.js
@@ -16,10 +16,32 @@ button.addEventListener("click", resize => {
 function generateGraph(size = 16) {
 for(let i = 0; i < (size * size); i++) {
     const div = document.createElement("div");
+    let opacity = 0
+    div.style.opacity = opacity;
+    div.classList.add("square");
     div.addEventListener("mouseenter", draw => {
-        div.classList.add("draw");
+        console.log("hovered");
+        let rgb = "rgb(" + getColor() + ")";
+        opacity += .1;
+        div.style.opacity  = opacity;
+        let currentOpacity = parseFloat(div.style.opacity);
+        console.log(currentOpacity);
+        div.style.backgroundColor = rgb;
+        
     });
     grid.appendChild(div);
 }
+}
+
+function getRandomNumberInRange(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getColor() {
+    let red = getRandomNumberInRange(0,255);
+    let green = getRandomNumberInRange(0,255);
+    let blue = getRandomNumberInRange(0,255);
+    return red + "," + green + "," + blue;
+    
 }
 generateGraph();
